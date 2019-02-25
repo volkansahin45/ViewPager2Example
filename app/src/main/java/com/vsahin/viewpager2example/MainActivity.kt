f42bba97dbe6490ef830e09ff83dcde9dc21456f
Volkan Şahin <nihas.naklov@gmail.com>
2019-02-25 19:08:12 +0300

    Initial Commit

---
+++ app/src/main/java/com/vsahin/viewpager2example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vsahin.viewpager2example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
